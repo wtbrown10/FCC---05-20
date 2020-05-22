b040a742ea923b71e54fa7791e36eff91cddfc55
Will Brown <wtbrown10@gmail.com>
2020-05-21 21:34:17 -0400

    solved

--- titlecase.js
+++ titlecase.js
@@ -1,5 +1,17 @@
 function titleCase(str) {
-  return str;
+  str = str.toLowerCase();
+  let newWord = "";
+
+  for (let i = 0; i < str.length; i++) {
+    newWord = newWord + str[i];
+    console.log(newWord);
+
+    if (str[i] == " ") {
+      newWord = newWord + str[i + 1].toUpperCase();
+      i++;
+    }
+    console.log(newWord);
+  }
 }
 
 titleCase("I'm a little tea pot");
