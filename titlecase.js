d2c5379ede00d112c272ac0c28fed7b7089a99d0
Will Brown <wtbrown10@gmail.com>
2020-05-21 21:57:33 -0400

    solved with less lines of code

--- titlecase.js
+++ titlecase.js
@@ -15,3 +15,18 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+// second way to solve
+
+function titleCase(str) {
+  let array = str.toLowerCase().split(" ");
+  let newSentence = "";
+
+  for (let i = 0; i < array.length; i++) {
+    newSentence += array[i][0].toUpperCase() + array[i].substr(1) + " ";
+  }
+  str = newSentence;
+  return str;
+}
+
+console.log(titleCase("I'm a little tea pot"));
