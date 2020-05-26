a52a5e8d80a0258f692d5ddfe530e723d6f67958
Will Brown <wtbrown10@gmail.com>
2020-05-25 21:26:01 -0400

    solved

--- mutation.js
+++ mutation.js
@@ -1,5 +1,17 @@
 function mutation(arr) {
-  return arr;
+  let word1 = arr[0].split("");
+  let word2 = arr[1].split("");
+
+  let count = 0;
+  for (let i = 0; i < word1.length; i++) {
+    for (let j = 0; j < word2.length; j++) {
+      if (word2[j] == word1[i]) {
+        count++;
+      }
+    }
+  }
+  let answer = Boolean(count === word2.length);
+  console.log(answer);
 }
 
-mutation(["hello", "hey"]);
+mutation(["animal", "pizza"]);
