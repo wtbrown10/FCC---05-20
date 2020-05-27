fb6c904e58f3651c3d67ecc4e34f4f359c8bd92a
Will Brown <wtbrown10@gmail.com>
2020-05-26 23:12:09 -0400

    while loop

--- chunkymonkey.js
+++ chunkymonkey.js
@@ -9,3 +9,15 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups([7, 3, 11, 21, 9], 2);
+
+function chunkArrayInGroups02(arr, size) {
+  const chunkArray = [];
+  let i = 0;
+  while (i < arr.length) {
+    chunkArray.push(arr.slice(i, i + size));
+    i += size;
+  }
+  console.log(chunkArray);
+}
+
+chunkArrayInGroups02([7, 3, 11, 21, 9, 4, 6, 9, 8, 12, 77, 69, 88], 3);
